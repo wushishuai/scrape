24fe8fef6b1ca0ef4fe06498d4eae9d44c1a12f9
wushishuai <32587860+wushishuai@users.noreply.github.com>
2021-12-09 19:04:30 +0800

    csv file storage 2

--- chapter4/csv file storage.py
+++ chapter4/csv file storage.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 with open('data.csv','w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['id','name','age'])
@@ -31,3 +32,21 @@ with open('data.csv','a',encoding='utf-8') as csvfile:
     filenames = ['id', 'name', 'age']
     writer = csv.DictWriter(csvfile, fieldnames=filenames)
     writer.writerow({'id': '10004', 'name': '王伟', 'age': 22})
+data = [
+    {'id':'10001','name':'Mike','age':20},
+    {'id':'10002','name':'Bob','age':22},
+    {'id':'10003','name':'Jordan','age':21},
+]
+df = pd.DataFrame(data)
+df.to_csv('data2.csv',index=False)
+
+with open('data.csv','r',encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+df = pd.read_csv('data2.csv')
+print(df)
+data = df.values.tolist()
+print(data)
+for index,row in df.iterrows():
+    print(row.tolist())

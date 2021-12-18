1addc9279ebbe09e65a313abe1a5fc4b4f767f96
wushishuai <32587860+wushishuai@users.noreply.github.com>
2021-12-18 16:18:26 +0800

    seleniun usage2

--- chapter7/selenium basic usage.py
+++ chapter7/selenium basic usage.py
@@ -4,7 +4,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver import ActionChains
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver import ChromeOptions
 import time
+
 '''基本用法'''
 # browser = webdriver.Chrome()
 # try:
@@ -66,12 +69,154 @@ import time
 
 '''动作链'''
 
-brower = webdriver.Chrome()
-url = ('https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable')
-brower.get(url)
-brower.switch_to.frame('iframeResult')
-source = brower.find_element(By.CSS_SELECTOR,'#draggable')
-target = brower.find_element(By.CSS_SELECTOR,'#droppable')
-actions = ActionChains(brower)
-actions.drag_and_drop(source,target)
-actions.perform()
+# brower = webdriver.Chrome()
+# url = ('https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable')
+# brower.get(url)
+# brower.switch_to.frame('iframeResult')
+# source = brower.find_element(By.CSS_SELECTOR,'#draggable')
+# target = brower.find_element(By.CSS_SELECTOR,'#droppable')
+# actions = ActionChains(brower)
+# actions.drag_and_drop(source,target)
+# actions.perform()
+
+
+'''运行javascript'''
+
+# brower = webdriver.Chrome()
+# brower.get('https://www.zhihu.com/explore')
+# brower.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+# brower.execute_script('alert("TO Botton")')
+
+
+'''获取节点信息'''
+
+# 获取属性
+
+# brower = webdriver.Chrome()
+# url = 'https://spa2.scrape.center/'
+# brower.get(url)
+# # log = brower.find_element(By.CLASS_NAME,'logo-image')
+# # print(log)
+# # print(log.get_attribute('src'))
+# #获取文本
+#
+# input = brower.find_element(By.CLASS_NAME,'logo-title')
+# print(input.text)
+# #获取ID,位置，标签名和大小
+#
+# print(input.id)
+# print(input.location)
+# print(input.tag_name)
+# print(input.size)
+
+
+'''切换Frame'''
+
+# brower = webdriver.Chrome()
+# url = 'http://www.runoob.com/try/try.php?filename=jqueryui-api-droppable'
+# brower.get(url)
+# brower.switch_to.frame('iframeResult')
+# try:
+#     logo = brower.find_element(By.CLASS_NAME, 'logo')
+# except NoSuchElementException:
+#     print('NO LOGO')
+# brower.switch_to.parent_frame()
+# logo = brower.find_element(By.CLASS_NAME, 'logo')
+# print(logo)
+# print(logo.text)
+
+'''延时等待'''
+
+# 隐式等待
+
+# browser = webdriver.Chrome()
+# browser.implicitly_wait(10)
+# browser.get('https://spa2.scrape.center/')
+# input = browser.find_element(By.CLASS_NAME,'logo-image')
+# print(input)
+
+# 显示等待
+
+
+# browser = webdriver.Chrome()
+# browser.get('https://www.taobao.com/')
+# wait = WebDriverWait(browser, 10)
+# input = wait.until(EC.presence_of_element_located((By.ID, 'q')))
+# button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn-search')))
+# print(input, button)
+
+'''前进和后退'''
+
+# browser = webdriver.Chrome()
+# browser.get('https://www.baidu.com/')
+# browser.get('https://www.taobao.com/')
+# browser.get('https://www.python.org/')
+# browser.back()
+# time.sleep(1)
+# browser.forward()
+# browser.close()
+
+'''cookie'''
+
+# browser = webdriver.Chrome()
+# browser.get('https://www.zhihu.com/explore')
+# print(browser.get_cookies())
+# browser.add_cookie({'name': 'name', 'domain': 'www.zhihu.com', 'value': 'germey'})
+# print(browser.get_cookies())
+# browser.delete_all_cookies()
+# print(browser.get_cookies())
+
+'''选项卡管理'''
+
+# browser = webdriver.Chrome()
+# browser.get('https://www.baidu.com')
+# browser.execute_script('window.open()')
+# print(browser.window_handles)
+# browser.switch_to.window(browser.window_handles[1])
+# browser.get('https://www.taobao.com')
+# time.sleep(1)
+# browser.switch_to.window(browser.window_handles[0])
+# browser.get('https://python.org')
+
+'''异常处理'''
+
+# browser = webdriver.Chrome()
+# browser.get('https://www.baidu.com')
+# browser.find_element(By.ID, 'hello')
+
+# brower = webdriver.Chrome()
+# try:
+#     brower.get('https://www.baidu.com/')
+# except TimeoutException:
+#     print('time out')
+# try:
+#     brower.find_element(By.ID, 'hello')
+# except NoSuchElementException:
+#     print('no element')
+# finally:
+#     brower.close()
+
+'''反屏蔽'''
+
+# browser = webdriver.Chrome()
+# browser.get('https://antispider1.scrape.center/')
+# time.sleep(1)
+# browser.close()
+
+# option = ChromeOptions()
+# option.add_experimental_option('excludeSwitches', ['enable-automation'])
+# option.add_experimental_option('useAutomationExtension', False)
+# browser = webdriver.Chrome(options=option)
+# browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
+#                         {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
+# browser.get('https://antispider1.scrape.center/')
+
+
+'''无头模式'''
+
+option = ChromeOptions()
+option.add_argument('--headless')
+browser = webdriver.Chrome(options=option)
+browser.set_window_size(1366, 768)
+browser.get('https://www.baidu.com')
+browser.get_screenshot_as_file('preview.png')

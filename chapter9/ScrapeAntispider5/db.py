7a71e04bd41afaf7bf15f4c3c910593cc19dc024
wushishuai <32587860+wushishuai@users.noreply.github.com>
2022-01-09 14:09:16 +0800

    scrapeantispider

---
+++ chapter9/ScrapeAntispider5/db.py
@@ -0,0 +1,39 @@
+from redis import StrictRedis
+from config import *
+from pickle import dumps, loads
+from request import MovieRequest
+
+
+class RedisQueue():
+    def __init__(self):
+        """
+        init redis connection
+        """
+        self.db = StrictRedis(
+            host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+
+    def add(self, request):
+        """
+        add request to queue
+        :param request: request
+        :param fail_time: fail times
+        :return: result
+        """
+        if isinstance(request, MovieRequest):
+            return self.db.rpush(REDIS_KEY, dumps(request))
+        return False
+
+    def pop(self):
+        """
+        get next request
+        :return: Request or None
+        """
+        if self.db.llen(REDIS_KEY):
+            return loads(self.db.lpop(REDIS_KEY))
+        return False
+
+    def clear(self):
+        self.db.delete(REDIS_KEY)
+
+    def empty(self):
+        return self.db.llen(REDIS_KEY) == 0
---
+++ chapter9/ScrapeAntispider5/request.py
@@ -0,0 +1,10 @@
+from config import *
+from requests import Request
+
+
+class MovieRequest(Request):
+    def __init__(self, url, callback, method='GET', headers=None, fail_time=0, timeout=TIMEOUT):
+        Request.__init__(self, method, url, headers)
+        self.callback = callback
+        self.fail_time = fail_time
+        self.timeout = timeout

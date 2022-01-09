2775363786ffe6a6499676810c3881c9e61ace7e
wushishuai <32587860+wushishuai@users.noreply.github.com>
2022-01-09 20:03:28 +0800

    JWT login

---
+++ chapter10/ScrapeLogin2/scrape.py
@@ -0,0 +1,28 @@
+import requests
+from urllib.parse import urljoin
+
+BASE_URL = 'https://login3.scrape.center/'
+LOGIN_URL = urljoin(BASE_URL, '/api/login')
+INDEX_URL = urljoin(BASE_URL, '/api/book')
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+response_login = requests.post(LOGIN_URL, data={
+    'username': USERNAME,
+    'password': PASSWORD
+})
+
+data = response_login.json()
+print('Response JSON', data)
+jwt = data.get('token')
+print('JWT', jwt)
+
+headers = {
+    'Authorization': f'jwt {jwt}'
+}
+
+response_index = requests.get(INDEX_URL, params={'limit': 18, 'offset': 0}, headers=headers)
+
+print('Response Status', response_index.status_code)
+print('Response URL', response_index.url)
+print('Response Data', response_index.json())

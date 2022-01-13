75ad6098feb966f7dadfce40ed0584d99dfaa1b7
wushishuai <32587860+wushishuai@users.noreply.github.com>
2022-01-13 11:07:47 +0800

    ATE test2

---
+++ chapter11/learn-ast/basic3.js
@@ -0,0 +1,26 @@
+import { parse } from "@babel/parser";
+import generate from "@babel/generator";
+import traverse from "@babel/traverse";
+
+
+import fs from "fs";
+
+const code = fs.readFileSync("codes/code1.js", "utf-8");
+let ast = parse(code);
+ traverse(ast, {
+   enter(path) {
+     let node = path.node;
+     if (node.type === "NumericLiteral" && node.value === 3) {
+       node.value = 5;
+     }
+     if (node.type === "StringLiteral" && node.value === "hello") {
+       node.value = "hi";
+//      console.log(path)
+     }
+   },
+ });
+
+const { code: output } = generate(ast, {
+  retainLines: true,
+});
+console.log(output);

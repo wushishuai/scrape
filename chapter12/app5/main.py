79a3c08e626282a3bc467888dc0849af5ef161fd
wushishuai <32587860+wushishuai@users.noreply.github.com>
2022-01-15 15:47:32 +0800

    ScrapeApp5

---
+++ chapter12/app5/main.py
@@ -0,0 +1,20 @@
+from appium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+server = 'http://localhost:4723/wd/hub'
+desired_capabilities = {
+    "platformName":"Android",
+    "deviceName":"M2006J10C",
+    "appPackage":"com.goldze.mvvmhabit",
+    "appActivity":".ui.MainActivity",
+    "noReset":True
+}
+
+driver = webdriver.Remote(server,desired_capabilities)
+wait = WebDriverWait(driver,30)
+wait.until(EC.presence_of_element_located((By.XPATH,'//android.support.v7.widget.RecyclerView/android.widget.LinearLayout')))
+window_size = driver.get_window_size()
+width, height = window_size.get('width'), window_size.get('height')
+driver.swipe(width * 0.5, height * 0.8, width * 0.5, height * 0.2, 1000)
